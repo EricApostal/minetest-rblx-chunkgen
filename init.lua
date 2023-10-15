bfde187ed5136a3fa0146f11cd972a5acf513560
Eric Apostal <ehapostal04@gmail.com>
2023-10-15 18:18:31 -0400

    It has to be async

--- init.lua
+++ init.lua
@@ -1,24 +1,46 @@
 minetest.log("Started!")
 
+
 local function getChunk(x, y)
     local chunkData = {}
 
-    local min = vector.new(x * 16, 0, y * 16)
-    local max = vector.new(x * 16 + 15, 256, y * 16 + 15)
+    local pos_min = vector.new(x * 16, 0, y * 16)
+    local pos_max = vector.new(x * 16 + 15, 256, y * 16 + 15)
 
-    local vm =  minetest.get_mapgen_object("voxelmanip")
-    local emin, emax = vm:read_from_map(min, max)
-    local area = VoxelArea:new{ MinEdge = emin, MaxEdge = emax }
-    local data = vm:get_data()
+    local len = 0
 
-    for z = min.z, max.z do
-        for y = min.y, max.y do
-            for x = min.x, max.x do
-                local vi = area:index(x, y, z)
-                minetest.log(data[vi])
+    local iters = 0
+    local function runGetChunkThing()
+        iters = iters + 1
+        minetest.log("Running callback: " .. iters)
+
+        if iters < 17 then
+            return
+        end
+
+        local voxelmanip = minetest.get_voxel_manip(pos_min, pos_max)
+        local emin, emax = voxelmanip:read_from_map(pos_min, pos_max)
+        local area = VoxelArea:new{ MinEdge = emin, MaxEdge = emax }
+        local data = voxelmanip:get_data()
+
+        for z = pos_min.z, pos_max.z do
+            for y = pos_min.y, pos_max.y do
+                for x = pos_min.x, pos_max.x do
+                    local blockType = minetest.get_name_from_content_id(data[area:index(x, y, z)])
+                    if not (blockType == "air") then
+                        local blockData = {x=x, y=y, z=z, bType=blockType}
+                        local blockHash = x..","..y..","..z
+                        chunkData[blockHash] = blockData
+                        len = len + 1
+                    end
+                end
             end
         end
+        minetest.log("Length: " .. len)
+        -- Now we can send the request back and whatnot
+
     end
+    minetest.emerge_area(pos_min, pos_max, runGetChunkThing)
 end
 
 minetest.register_chatcommand("cc", {
@@ -29,7 +51,7 @@ minetest.register_chatcommand("cc", {
         local player = minetest.get_player_by_name(name)
         local pos = player:get_pos()
         -- getChunk(math.floor(pos.x / 16), math.floor(pos.z / 16))
-        getChunk(math.floor(1000), math.floor(1000))
+        getChunk(1000, 1000)
     end,
 })
 
